901ccf3e7ebc2df68b9b829847ae5a9d575f7ed3
fauz ali <fauz_sp@hotmail.com>
2025-02-25 20:24:23 +0500

    demo folder added

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,13 +1,28 @@
-// import React from "react";
-// import { ToastContainer } from "react-smart-toast";
+import { ToastContainer } from "react-smart-toast";
 
-// const App = () => {
-//   return (
-//     <div>
-//       <h1>React Smart Toast Demo</h1>
-//       <ToastContainer />
-//     </div>
-//   );
-// };
+function App() {
+  return (
+    <div>
+      <h1>React Smart Toast Demo</h1>
+      <h2>hello world</h2>
+      <ToastContainer
+          defaultToasts={[
+            { id: Date.now(), message: "Operation Successful!", type: "success" },
+            { id: Date.now() + 1, message: "Something went wrong!", type: "error" },
+            { id: Date.now() + 2, message: "Here’s some info for you!", type: "info" },
+            { id: Date.now() + 3, message: "Warning! Check your input.", type: "warning" },
+          ]}
+          customStyles={{
+            position: "fixed",
+            bottom: "0",
+            right: "10px",
+            display: "flex",
+            flexDirection: "column",
+            gap: "10px",
+          }}
+        />
+    </div>
+  );
+}
 
-// export default App;
+export default App;
--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,11 @@
-import './App.css';
-import ToastContainer from './components/ToastContainer';
-import logo from './logo.svg';
+import ToastContainer from "./components/ToastContainer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <ToastContainer
-          defaultToasts={[
-            { id: Date.now(), message: "Operation Successful!", type: "success" },
-            { id: Date.now() + 1, message: "Something went wrong!", type: "error" },
-            { id: Date.now() + 2, message: "Here’s some info for you!", type: "info" },
-            { id: Date.now() + 3, message: "Warning! Check your input.", type: "warning" },
-          ]}
-          customStyles={{  
-            position: "fixed",
-            bottom: "0",
-            display: "flex",
-            right: "10px",
-            flexDirection: "column",
-            gap: "10px",
-          }}
-        />
-      </header>
+    <div>
+      <h1>React Smart Toast (Test Mode)</h1>
+      <p>This file is only for local testing.</p>
+      <ToastContainer />
     </div>
   );
 }
